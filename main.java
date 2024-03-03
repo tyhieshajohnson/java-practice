1a55b0464875b7503ebdd60f5219bf0dcffc2728
tyhiesha johnson <140961082+tyhieshajohnson@users.noreply.github.com>
2024-03-03 19:29:12 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+//What is Java?
+//Java is a popular programming language, created in 1995.
+//It is owned by Oracle, and more than 3 billion devices run Java.
+
+//Why Java?
+//Java is an object oriented language which gives a clear structure to programs and allows code to be reused, lowering development costs.
+//Java works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc.)
+
+//Getting Started:
+
+// Declaration of a public class named Main
+public class Main {
+  
+  // The main method, the entry point of the program
+  public static void main(String[] args) {
+    
+    // Printing "Hello World" to the console
+    System.out.println("Hello World");
+  }
+}
