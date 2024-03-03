f82496b2aa5fd3d4d1ca2bdf120f225e0798383b
tyhiesha johnson <140961082+tyhieshajohnson@users.noreply.github.com>
2024-03-03 19:38:59 +0200

    Create syntax.java

---
+++ syntax.java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+//Notes:
+//1. Every line of code that runs in Java must be inside a class. 
+//In our example, we named the class Main. A class should always start with an uppercase first letter.
+
+//2. The name of the java file must match the class name. When saving the file, save it using the class name and add ".java" to the end of the filename.
+
+//The main Method
+//a. The main() method is required and you will see it in every Java program:
+//public static void main(String[] args)
+//Any code inside the main() method will be executed.
+
+//System.out.printIn()
+//Inside the main() method, we can use the println() method to print a line of text to the screen:
+//public static void main(String[] args) {
+//  System.out.println("Hello World");
+//}
+
